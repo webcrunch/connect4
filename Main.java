1eaeafadb31cd99ad1d73add148e8dc4c46c71e4
jarl Lindquist <jarllinduist@gmail.com>
2022-09-05 10:42:32 +0200

    start with adding help methods

--- Main.java
+++ Main.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+       int player1 =  choosePlayers("Player 1: Are you a human or bot?");
+        System.out.println(player1);
+    }
+
+    private static int choosePlayers(String question){
+        Scanner inputFromUsers = new Scanner(System.in);
+        print(question, "1. Human", "2. crazy bot", "3. advanced bot");
+
+        int userChoose = Integer.parseInt(inputFromUsers.nextLine());
+        return userChoose;
+    }
+
+
+    private static void print(String question, String... choices){
+        print(question);
+        int number = 0;
+        for (String choice : choices) {
+            print(choice);
+            number++;
+        }
+    }
+
+
+    private static void print(String message){
+        System.out.println(message);
+    }
 }
