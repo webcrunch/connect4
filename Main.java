8117747fd7946def6d524d56598958d2a09c29ad
jarl Lindquist <jarllinduist@gmail.com>
2022-09-05 11:43:01 +0200

    adding a start play grafik for the game

--- Main.java
+++ Main.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Introduction for the user
+        print("\n".repeat(60));
+        print("Connect4 game");
+        print("-".repeat(60));
+        print("Welcome to Connect4. Rules are easy");
+        print(" ".repeat(1));
+        // ta kod från Thomas valKompassen
        int player1 =  choosePlayers("Player 1: Are you a human or bot?");
-        System.out.println(player1);
+        int player2 =  choosePlayers("Player 1: Are you a human or bot?");
+        System.out.println(player1 + " " + player2);
     }
 
     private static int choosePlayers(String question){
         Scanner inputFromUsers = new Scanner(System.in);
-        print(question, "1. Human", "2. crazy bot", "3. advanced bot");
+        print(question, "Human", "Crazy bot", "Advanced bot");
 
         int userChoose = Integer.parseInt(inputFromUsers.nextLine());
         return userChoose;
@@ -17,9 +25,9 @@ public class Main {
 
     private static void print(String question, String... choices){
         print(question);
-        int number = 0;
+        int number = 1;
         for (String choice : choices) {
-            print(choice);
+            print(number + " " + choice);
             number++;
         }
     }
