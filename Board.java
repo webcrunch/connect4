29bd3ce409e164fcb3460db06c23f31a2b7e7194
jarl Lindquist <jarllinduist@gmail.com>
2022-09-09 13:15:59 +0200

    adding changes of player after each actions

--- Board.java
+++ Board.java
@@ -35,9 +35,33 @@ public class Board {
         }
     }
 
+ /*   private static int occupiedOrNot(int choose){
+        int row = -1;
+        do{
+            board[choose][4].equals(" ")
+        }while(row < 0)
+            return 2;
+    }
+    public static void crazyBotDropToBoard(){
+
+    } */
+
     public static void dropToBoard(String player, int row){
-        System.out.println(player);
-        System.out.println(row);
+        int minC = 0;
+        int maxC = 7;
+        int minR = 0;
+        int maxR = 6;
+        int random_intCol = (int)Math.floor(Math.random()*(maxC-minC+1)+minC);
+        int random_intRow = (int)Math.floor(Math.random()*(maxR-minR+1)+minR);
+        System.out.println(random_intRow + " <-row col-> " +  random_intCol);
+        try{
+            //String color = Players.colorOfPlayer(player);
+            //System.out.println(color + " dddddddd");
+            board[random_intRow][random_intCol] = player ;
+            Players.changeCurrentPlayer();
+        }
+        catch (Exception ignore){}
+
         // todo check the rows column witch (rowcolumn) that is free.
     }
 }
--- Players.java
+++ Players.java
@@ -10,15 +10,25 @@ public class Players {
     public static String Color1 = ConsoleColors.GREEN;
     public static String Color2 = ConsoleColors.YELLOW;
     public static String currentPlayer = Player1;
+
+
+    public static  String colorOfPlayer(String player){
+        return Player1.equals(player) ? Color1 : Color2;
+    }
     // public static String whoIsCurrentPlayer(){
     //    System.out.println(Player1);
     //    return player1.currentPlayer ? player2 : player1;
     //}
 
     public  static void changeCurrentPlayer(){
-        System.out.println(currentPlayer);
-        System.out.println(Player1);
-        currentPlayer = Player1.equals(currentPlayer) ? Player2 : Player2;
+       /* System.out.println(currentPlayer + " Current Player ");
+        System.out.println(Player1 + " Player1 ");
+        System.out.println(Player1.equals(currentPlayer));
+        System.out.println(Player1 == currentPlayer ? Player2 : Player2); */
+
+
+        //currentPlayer = Player1.equals(currentPlayer) ? Player2 : Player1;
+        currentPlayer = Player1 == currentPlayer ? Player2 : Player1;
     }
     public static String choosePlayers(String question) {
         Scanner inputFromUsers = new Scanner(System.in);
