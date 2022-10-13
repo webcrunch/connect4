36146f619fb68ce89c4c2845555294c8d8460c92
jarl Lindquist <jarllinduist@gmail.com>
2022-10-13 13:52:51 +0200

    add different color codes to the color class.

--- ConsoleColors.java
+++ ConsoleColors.java
@@ -2,6 +2,16 @@ public class ConsoleColors {
     // Reset
     public static final String RESET = "\033[0m";  // Text Reset
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     // Regular Colors
     public static final String BLACK = "\033[0;30m";   // BLACK
     public static final String RED = "\033[0;31m";     // RED
